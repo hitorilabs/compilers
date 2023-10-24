a88e9b1f2c782001ef43b199d1af8f4381bad733
hitorilabs <131238467+hitorilabs@users.noreply.github.com>
2023-10-24 22:26:21 +0000

    make the implementation feel more like C

--- examples/recursive_translator.c
+++ examples/recursive_translator.c
@@ -2,55 +2,78 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-int lookahead;
+//  Grammar:
+//      expr   -> term rest
+//      rest   -> + term {print '+'} 
+//              | - term {print '-'} 
+//              | ε
+//      term   -> 0 { print '0' }
+//              | 1 { print '1' }
+//                ...
+//              | 9 { print '9' }
 
-void expr();
-void term();
-void match(int t);
+void get_expr();
+void term(int* lookahead);
+void match(int terminal, int* lookahead);
 
-void expr() {
-    term();
+void get_expr() {
+    int lookahead;
+
+    lookahead = getchar();
+    if (lookahead == EOF) {
+        fprintf(stderr, "error reading stdin\n");
+        exit(1);
+    }
+    term(&lookahead);
+
     while (1) {
-        if (lookahead == '+') {
-            match('+');
-            term();
-            putchar('+');
-        } else if (lookahead == '-') {
-            match('-');
-            term();
-            putchar('-');
-        } else {
-            return;
+        switch (lookahead) {
+            case EOF:
+                return;
+            case '+':
+                match('+', &lookahead);
+                term(&lookahead);
+                putchar('+');
+                break;
+            case '-':
+                match('-', &lookahead);
+                term(&lookahead);
+                putchar('-');
+                break;
+            default:
+                putchar('\n');
+                fprintf(stderr, "syntax error: invalid grammar\n");
+                exit(1);
         }
     }
 }
 
-void term() {
-    if (isdigit(lookahead)) {
-        putchar(lookahead);
-        match(lookahead);
+// check if lookahead is a term
+void term(int* lookahead) {
+    if (isdigit(*lookahead)) {
+        putchar(*lookahead);
+        match(*lookahead, lookahead);
     } else {
-        fprintf(stderr, "syntax error\n");
+        fprintf(stderr, "syntax error: expected a digit\n");
         exit(1);
     }
 }
 
-void match(int t) {
-    if (lookahead == t) {
-        lookahead = getchar();
+// if lookahead symbol matches an expected terminal symbol
+// * get the next (non-whitespace) lookahead symbol
+void match(int terminal, int* lookahead) {
+    if (terminal == *lookahead) {
+        do {
+            *lookahead = getchar();
+        } while (isspace(*lookahead));
     } else {
-        fprintf(stderr, "syntax error\n");
+        fprintf(stderr, "syntax error: expected (%c) got (%c)\n", terminal, *lookahead);
         exit(1);
     }
 }
 
 int main() {
-    lookahead = getchar();
-    if (lookahead == EOF) {
-        fprintf(stderr, "Error reading input.\n");
-        return 1;
-    }
-    expr();
+    get_expr();
     return 0;
 }
 
